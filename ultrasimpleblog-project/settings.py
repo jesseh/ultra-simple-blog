33eaf536e97e771650cc855dc114e4d45306cbea
Jesse <jesse.heitler@torqueim.com>
2011-09-20 00:43:16 +0100

    Include STATIC_URL processor

--- ultrasimpleblog-project/settings.py
+++ ultrasimpleblog-project/settings.py
@@ -100,6 +100,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
     'django.core.context_processors.media',
+    'django.core.context_processors.static',
 )
 
 # This test runner captures stdout and associates tracebacks with their
