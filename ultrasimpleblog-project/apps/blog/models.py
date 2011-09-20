f7059b086b8c09fb1d2e582b1dc4509c2cb2727e
Jesse <jesse.heitler@torqueim.com>
2011-09-20 12:48:23 +0100

    Removed uniqueness check happen django handles.
    Django nonrel seems to run a uniqueness check on the model form. The
    documentation is a bit confusing in this area.

--- ultrasimpleblog-project/apps/blog/models.py
+++ ultrasimpleblog-project/apps/blog/models.py
@@ -7,31 +7,18 @@ from model_utils.models import TimeStampedModel
 
 class Article(TimeStampedModel):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True) # Unique is ignored in non-rel. Enforced at save.
+    slug = models.SlugField(unique=True)
     content = models.TextField()
     published = models.BooleanField()
     published_date = models.DateTimeField(blank=True, null=True,
             help_text="The published date is set automatically when this is first published.")
 
-    def validate_unique(self, *args, **kwargs):
-        """
-        Directly validate that the slug is unique.
-        @@@ Is this the right way to do a uniqueness check in nonrel?
-        """
-        if self.id is None:
-            try:
-                article = Article.objects.get(slug=self.slug)
-                raise ValidationError("The slug '%s' is already in use. Please make it unique." % self.slug)
-            except Article.DoesNotExist:
-                pass
-
     def clean(self, *args, **kwargs):
         """
-        Set the publish date before saving.
+        Set the publish date before saving and keep the slug unique.
         """
         if self.published and not self.published_date:
             self.published_date = datetime.now()
-        super(Article, self).save(*args, **kwargs)
 
     @models.permalink
     def get_absolute_url(self):
